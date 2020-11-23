39961272571a68d34bf26aa5f5c63a998ced6969
Dmitry <bens1iller@gmail.com>
2020-11-23 13:13:19 +0300

    working version

---
+++ build/precache-manifest.a99cd908f4908e830265ac57c5df9d93.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48bc4ba18b56acf466a2ffcc99b2b1aa",
+    "url": "/weather-forecast/index.html"
+  },
+  {
+    "revision": "86ef2950dbacbd129c14",
+    "url": "/weather-forecast/static/css/main.d3dc427f.chunk.css"
+  },
+  {
+    "revision": "98ca85d8b017e2d7906a",
+    "url": "/weather-forecast/static/js/2.6c1ff8e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-forecast/static/js/2.6c1ff8e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86ef2950dbacbd129c14",
+    "url": "/weather-forecast/static/js/main.dec30189.chunk.js"
+  },
+  {
+    "revision": "a96cd891956dfff3f9cf",
+    "url": "/weather-forecast/static/js/runtime-main.d4e096a0.js"
+  },
+  {
+    "revision": "13b7ce35343b91894a824263bcbafa56",
+    "url": "/weather-forecast/static/media/city-marker.13b7ce35.svg"
+  },
+  {
+    "revision": "540498c4bd4582738e76efbf539f9855",
+    "url": "/weather-forecast/static/media/graphic-day.540498c4.svg"
+  },
+  {
+    "revision": "b1ec981050006a9422af77703d07600a",
+    "url": "/weather-forecast/static/media/graphic-night.b1ec9810.svg"
+  },
+  {
+    "revision": "2ef4cc1fa4002ce5897e9befabf2abf6",
+    "url": "/weather-forecast/static/media/search-town.2ef4cc1f.svg"
+  }
+]);
---
+++ src/components/Info.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Info extends React.Component {
+    render() {
+        return (
+            <div className="info">Является моей интеллектуальной собственностью</div>
+        )
+    }
+}
+
+export default Info;
